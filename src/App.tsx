abd88170f307479e3f09c1d8295f960e821df980
Nicholas Ingulfsen <ingulfsn@gmail.com>
2023-02-08 19:02:18 +0100

    Added messages

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,8 @@ const App: React.FC = () => {
   const [connectionState, setConnectionState] =
     useState<ConnectionState>("connecting");
   const [counter, setCounter] = useState(0);
+  const [messages, setMessages] = useState<string[]>([]);
+  const [inputText, setInputText] = useState("");
   const signaling = useRef(new Signaling()).current;
 
   const onConnected = () => {
@@ -18,6 +20,10 @@ const App: React.FC = () => {
     setCounter(count);
   };
 
+  const onMessagesUpdate = (data: string[]) => {
+    setMessages(data);
+  };
+
   useEffect(() => {
     const connect = async () => {
       await signaling.connect();
@@ -25,6 +31,7 @@ const App: React.FC = () => {
       if (socket) {
         socket.on("connect", onConnected);
         socket.on("counter_update", onCounterChanged);
+        socket.on("messages_update", onMessagesUpdate);
       }
     };
 
@@ -36,19 +43,55 @@ const App: React.FC = () => {
     signaling.requestPromise("count");
   };
 
+  const onSendMessageAction = (message: string) => {
+    signaling.requestPromise("send_message", message);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      if (inputText.trim() !== "") {
+        onSendMessageAction(inputText);
+      }
+      setInputText("");
+    }
+  };
+
   return (
-    <div>
+    <div className="w-screen h-screen bg-slate-200 flex items-center justify-center">
+      {/* Connecting */}
       {connectionState === "connecting" && <div>Connecting...</div>}
+      {/* Main content */}
       {connectionState === "connected" && (
-        <div>
-          {counter}
-          <div>
-            <span
-              className="border rounded-md cursor-pointer"
+        <div className="flex flex-col items-center justify-center">
+          {/* Counter */}
+          <div className="flex flex-col items-center justify-center">
+            <div
+              className="text-8xl text-slate-600 font-thin mb-4 cursor-pointer select-none"
               onClick={onCountAction}
             >
-              Count
-            </span>
+              {counter}
+            </div>
+          </div>
+          {/* Messages */}
+          <div className="flex flex-col items-center justify-center">
+            <div className="relative w-80">
+              <div className="flex flex-col space-y-4 w-full max-h-96 justify-end overflow-hidden">
+                {messages.map((m) => {
+                  return (
+                    <div className="bg-slate-300 text-slate-600 px-4 py-3 rounded-xl">
+                      {m}
+                    </div>
+                  );
+                })}
+              </div>
+              <div className="absolute top-0 right-0 left-0 h-40 from-slate-200 bg-gradient-to-b" />
+            </div>
+            <input
+              className="w-full h-10 mt-8 px-3 py-2 focus:outline-0 bg-slate-100 rounded-lg"
+              onKeyDown={handleKeyDown}
+              value={inputText}
+              onChange={(e) => setInputText(e.target.value)}
+            />
           </div>
         </div>
       )}
